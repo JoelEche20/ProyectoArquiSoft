b037194a470005cf86be1d6d72144b9ba424fa01
JoelEche20 <joel_ma_pa@hotmail.com>
2020-12-15 16:53:07 -0400

    Added home component.

--- library-ui/libraryUi/src/app/app.module.ts
+++ library-ui/libraryUi/src/app/app.module.ts
@@ -5,20 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatTableModule} from '@angular/material/table';
+
 import { HeaderComponent } from '../app/components/header/header.component';
+import { HomeComponent } from '../app/components/home/home.component';
 import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
